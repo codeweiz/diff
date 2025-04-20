69680256667f393af98f0385271a4c5bee22434f
zhouwei <zhouwei@chances.com.cn>
2025-04-20 15:03:23 +0800

    feat(init): 项目初始化

---
+++ src/test/java/cn/microboat/diff/DiffApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.microboat.diff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
